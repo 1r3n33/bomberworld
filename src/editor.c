3c382fbb0689b2f9eb85d864f39d67da9944fb66
Irenee <icaroulle@gmail.com>
2020-02-26 22:32:27 +0100

    Add constraints when adding blocks in editor

--- src/editor.c
+++ src/editor.c
@@ -10,6 +10,15 @@ extern char spr_editor_pal_begin, spr_editor_pal_end;
 
 u16 editor_tilemap[32][32];
 
+// Keep cursor inside the safe frame
+#define SAFE_FRAME_TOP    (9*8)
+#define SAFE_FRAME_BOTTOM (25*8)
+#define SAFE_FRAME_LEFT   (1*8)
+#define SAFE_FRAME_RIGHT  (30*8)
+
+#define SPR_EDITOR_CURSOR_VALID     0
+#define SPR_EDITOR_CURSOR_INVALID   2
+
 struct cursor_t
 {
     u8  spr;
@@ -39,57 +48,91 @@ u16 * get_editor_tilemap()
 void init_cursor(u8 id)
 {
     cursors[id].spr   = id * 4;
-    cursors[id].x     = (id == 0) ? 8 : (256-16);
-    cursors[id].y     = 25*8;
-    cursors[id].state = 1;
+    cursors[id].x     = (id == 0) ? SAFE_FRAME_LEFT : SAFE_FRAME_RIGHT;
+    cursors[id].y     = SAFE_FRAME_BOTTOM;
+    cursors[id].state = SPR_EDITOR_CURSOR_VALID;
 
-    oamSet(cursors[id].spr, 0xFF, 0xFF, 3, 0, 0, 0, 0);
+    oamSet(cursors[id].spr, 0xFF, 0xFF, 3, 0, 0, cursors[id].state, 0);
     oamSetEx(cursors[id].spr, OBJ_SMALL, OBJ_SHOW);
 }
 
-void move_cursor(u8 id, u16 pad)
+void update_cursor(u8 id, u16 pad)
 {
+    // Update position
     if (pad & KEY_UP)
     {
         cursors[id].y -= 8;
-        if (cursors[id].y < (25-16)*8)
+        if (cursors[id].y < SAFE_FRAME_TOP)
         {
-            cursors[id].y = (25-16)*8;
+            cursors[id].y = SAFE_FRAME_TOP;
         }
     }
 
     if (pad & KEY_DOWN)
     {
         cursors[id].y += 8;
-        if (cursors[id].y > (25*8))
+        if (cursors[id].y > SAFE_FRAME_BOTTOM)
         {
-            cursors[id].y = (25*8);
+            cursors[id].y = SAFE_FRAME_BOTTOM;
         }
     }
 
     if (pad & KEY_LEFT)
     {
         cursors[id].x -= 8;
-        if (cursors[id].x < 8)
+        if (cursors[id].x < SAFE_FRAME_LEFT)
         {
-            cursors[id].x = 8;
+            cursors[id].x = SAFE_FRAME_LEFT;
         }
     }
 
     if (pad & KEY_RIGHT)
     {
         cursors[id].x += 8;
-        if (cursors[id].x > (256-16))
+        if (cursors[id].x > SAFE_FRAME_RIGHT)
         {
-            cursors[id].x = (256-16);
+            cursors[id].x = SAFE_FRAME_RIGHT;
         }
     }
 
+    // Check validity
+    // If the cursor is on the bottom line, always OK as we start building up blocks from the ground.
+    // If the cursor is above the bottom line, check if there is a block below or under the cursor.
+    // This last rule makes edition a little less fun...
+    if (cursors[id].y == SAFE_FRAME_BOTTOM)
+    {
+        cursors[id].state = SPR_EDITOR_CURSOR_VALID;
+    }
+    else
+    {
+        u8 x = cursors[id].x/8;
+        u8 y = cursors[id].y/8;
+
+        u16 under = editor_tilemap[y][x];
+        u16 below = editor_tilemap[y+1][x];
+
+        if (under | below)
+        {
+            cursors[id].state = SPR_EDITOR_CURSOR_VALID;
+        }
+        else
+        {
+            cursors[id].state = SPR_EDITOR_CURSOR_INVALID;
+        }
+    }
+
+    // Update display
+    oamSet(cursors[id].spr, 0xFF, 0xFF, 3, 0, 0, cursors[id].state, 0);
     oamSetXY(cursors[id].spr, cursors[id].x, cursors[id].y);
 }
 
 void add_block(u8 id)
 {
+    if (cursors[id].state == SPR_EDITOR_CURSOR_INVALID)
+    {
+        return;
+    }
+
     u8 x = cursors[id].x/8;
     u8 y = cursors[id].y/8;
 
@@ -106,6 +149,11 @@ void add_block(u8 id)
 
 void remove_block(u8 id)
 {
+    if (cursors[id].state == SPR_EDITOR_CURSOR_INVALID)
+    {
+        return;
+    }
+
     u8 x = cursors[id].x/8;
     u8 y = cursors[id].y/8;
 
@@ -150,8 +198,8 @@ u8 run_editor()
             u16 pad0 = padsCurrent(0);
             u16 pad1 = padsCurrent(1);
 
-            move_cursor(0, pad0);
-            move_cursor(1, pad1);
+            update_cursor(0, pad0);
+            update_cursor(1, pad1);
 
             if (pad0 & KEY_A)
             {
